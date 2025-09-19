4cdbf12fbb405aeec7b13fa935e777290ca87cb8
IamMax279 <maksssiula@gmail.com>
2025-09-19 18:06:03 +0200

    finish multinomial NB implementation

--- src/models/naive_bayes.py
+++ src/models/naive_bayes.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 def train_naive_bayes(X, y):
     cv = CountVectorizer()
@@ -23,11 +25,38 @@ def train_naive_bayes(X, y):
     # [[1],
     #  [2],
     #  [3]]
-    likelihood = (word_counts_per_class + 1) / (total_words_per_class[:, None] + X_mat.shape[1])
+    likelihoods = (word_counts_per_class + 1) / (total_words_per_class[:, None] + X_mat.shape[1])
+    return cv, priors, likelihoods
+
+def predict(X, cv, priors, likelihoods):
+    X_mat = cv.transform(X).toarray()
     
+    # when calculating likelihoods, we might be multiplying small numbers like
+    # 0.001 * 0.002 * ... - hence we use np.log() because the logarithm
+    # will convert multiplication to addition and thus, we'll avoid underflow
+    log_likelihoods = np.log(likelihoods)
+    posteriors = X_mat @ log_likelihoods.T + priors
+
+    predictions = np.argmax(posteriors, axis=1)
+    return predictions
+
+def get_accuracy(y, predictions, plot_c_mat=True, labels=[0, 1]):
+    y_vec = np.array(y)
+    if plot_c_mat:
+        cm = confusion_matrix(y_vec, predictions.flatten())
+        disp = ConfusionMatrixDisplay(cm, display_labels=labels)
+
+        disp.plot(cmap='Blues')
+        plt.show()
+    
+    return np.mean(y_vec == predictions.flatten())
+    # alternatively:
+    # return (y_vec == predictions.flatten()).sum(axis=0) / y_vec.shape[0]
 
 df = pd.read_csv('src/data/spam.csv', sep=',')
 df['Category'] = df['Category'].apply(lambda x: 1 if x == 'spam' else 0)
 X_train, X_test, y_train, y_test = train_test_split(df['Message'], df['Category'], test_size=0.25, random_state=42)
 
-train_naive_bayes(X_train, y_train)
+cv, priors, likelihoods = train_naive_bayes(X_train, y_train)
+predictions = predict(X_test, cv, priors, likelihoods)
+score = get_accuracy(y_test, predictions)
